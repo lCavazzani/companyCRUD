ead061d4ca7a6595a4f56ac8a73fe695eca23d54
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-12-01 16:01:00 -0600

    login and register layout

--- app/src/main/java/com/faculdade/companycrud/LoginActivity.java
+++ app/src/main/java/com/faculdade/companycrud/LoginActivity.java
@@ -1,13 +1,35 @@
 package com.faculdade.companycrud;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button BTNLogin;
+    Button BTNRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        BTNLogin = findViewById(R.id.Btnlogin);
+        BTNRegister = findViewById(R.id.Btnregister);
+
+        BTNLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
+        BTNRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
